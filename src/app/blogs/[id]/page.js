dda7070c4633ec0b14234547a11968de12d26fd7
efecansengul <efecansengul33@gmail.com>
2024-03-13 21:37:34 +0300

    created blog detail page.

---
+++ src/app/blogs/[id]/page.js
@@ -0,0 +1,17 @@
+import styles from "./blog-detail-page.module.css";
+
+async function BlogDetailPage({ params }) {
+  return (
+    <div className={styles.postWrapper}>
+      <h2 className={styles.title}>title</h2>
+      <p className={styles.author}>
+        <span>Written by:</span>
+        efecansengul
+      </p>
+      <p className={styles.content}>askdljflşkasdklfjlşskajdfşlkjsaşldk</p>
+      {/*<Comments postId={params.id} />
+      <FormComments postId={params.id} />*/}
+    </div>
+  );
+}
+export default BlogDetailPage;
