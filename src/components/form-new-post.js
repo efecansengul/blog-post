25787393b6c4b2ecfa908c79d5794e8190f84a10
efecansengul <efecansengul33@gmail.com>
2024-03-13 22:31:34 +0300

    created provider comp. and getUser.

--- src/components/form-new-post.js
+++ src/components/form-new-post.js
@@ -2,6 +2,7 @@
 import ReactTextareaAutosize from "react-textarea-autosize";
 import styles from "./form-new-post.module.css";
 import { useState } from "react";
+import { useSession } from "next-auth/react";
 
 function FormNewPost() {
   const [loading, setLoading] = useState(false);
@@ -12,6 +13,12 @@ function FormNewPost() {
     content: "",
   });
 
+  //kullanıcı bilgisi
+  const { data } = useSession();
+  let user = <p>Welcome user.Please Login</p>;
+  if (data?.user) {
+    user = <p>Welcome {data.user.name}</p>;
+  }
   function handleChange(event) {
     setError(false);
     setFormData((prevState) => ({
@@ -27,6 +34,7 @@ function FormNewPost() {
   return (
     <>
       <form className={styles.form} onSubmit={handleSubmit}>
+        {user}
         <div className={styles.inputWrapper}>
           <input
             type="text"
